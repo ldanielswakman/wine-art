b34e2a1765d2ef4dc7c211bd05c211761043c57e
L Daniel Swakman <d.swakman@gmail.com>
2016-12-08 09:55:52 +0300

    small things

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,1 +1,16 @@
-<?= $page->text() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <title><?= $site->title() ?></title>
+
+    <?= css('assets/css/style.css') ?>
+
+  </head>
+
+  <body>
+    <p><?= $page->text()->html() ?></p>
+  </body>
+
+</html>
