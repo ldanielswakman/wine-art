b2bca18ae9a0de61f9580057734b56f844927c1e
L Daniel Swakman <d.swakman@gmail.com>
2016-12-14 11:36:53 +0300

    content / site update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,19 @@
     <?= css('assets/css/flexboxgrid.min.css') ?>
     <?= css('assets/css/style.css') ?>
 
+    <?= js('assets/js/jquery-2.2.3.min.js') ?>
+    <?= js('assets/js/scripts.js') ?>
+
   </head>
 
   <body>
+
+    <header class="u-aligncenter u-pv6">
+
+      <? if($logo = $site->logo()): ?>
+        <img src="<?= $site->image($logo)->url() ?>" class="header__logo" />
+      <? else : ?>
+        <?= $site->title() ?>
+      <? endif ?>
+
+    </header>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,21 +1,29 @@
 <? snippet('header') ?>
 
+<? $i = 0; ?>
 <? foreach ($page->children()->visible() as $section): ?>
-  <section id="<?= $section->slug() ?>">
-    <div class="row">
 
+  <? $uneven = ($i%2 != 0) ? true : false; ?>
+
+  <section class="u-mb8" id="<?= $section->slug() ?>">
+    <div class="row">
+      <? $image_class = $uneven ? 'col-sm-5 last-sm col-sm-offset-1' : 'col-sm-5'; ?>
       <? if($image = $section->image()): ?>
-        <div class="col-xs-10 col-sm-5 u-minheight20 u-relative" style="background-image: url('<?= $image->url() ?>');"></div>
+        <div class="col-xs-10 <?= $image_class ?> u-minheight20 u-relative section__bg-image" style="background-image: url('<?= $image->url() ?>');"></div>
       <? endif ?>
 
-      <div class=" col-xs-12 col-sm-7 u-pl3">
-        <h4><?= $section->title() ?></h4>
+      <? $text_class = $uneven ? 'col-sm-5 col-sm-offset-1' : 'col-sm-7'; ?>
+      <div class="col-xs-12 <?= $text_class ?> u-pl3 u-pb6">
+        <!-- <h4><?= $section->title() ?></h4> -->
 
         <?= kirbytext($section->text()) ?>
       </div>
 
     </div>
   </section>
+
+  <? $i++; ?>
+
 <? endforeach; ?>
 
 <? snippet('footer') ?>
