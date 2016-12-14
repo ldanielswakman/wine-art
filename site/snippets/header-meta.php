40c0a4cec4b3cc1a743cefa9f681174465e5be0a
L Daniel Swakman <d.swakman@gmail.com>
2016-12-14 12:08:41 +0300

    metadata

--- site/snippets/header-meta.php
+++ site/snippets/header-meta.php
@@ -1,13 +1,14 @@
+
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta name="author" content="L Daniel Swakman, ldaniel.eu" />
 <meta http-equiv="Cache-control" content="public">
 
-<meta name="description" content="<?= $description ?>">
+<meta name="description" content="<?= $site->description()->html() ?>">
 <meta name="keywords" content="<?= $site->keywords()->html() ?>">
 
 <!-- Social share parameters -->
-<meta property="og:image" content="<?= (isset($meta_image)) ? $meta_image : $meta_image_url; ?>" />
+<meta property="og:image" content="<?= ($site->meta_image()->isNotEmpty()) ? $site->image($site->meta_image())->url() : '' ?>" />
 <meta property="og:title" content="<?= $page->title() ?>" />
 <meta property="og:site_name" content="<?= $site->title() ?>" />
-<meta property="og:description" content="<?= $description ?>" />
+<meta property="og:description" content="<?= $site->description()->html() ?>" />
