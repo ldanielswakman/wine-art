37c98aea5a67979e40cc707a26b24d7be23de061
L Daniel Swakman <d.swakman@gmail.com>
2017-02-10 17:51:40 -0300

    services page tweak

--- site/templates/list.php
+++ site/templates/list.php
@@ -2,49 +2,58 @@
 
 <main>
 
+  <?
+  $bg_image_style = '';
+  if($page->cover_image()->isNotEmpty()) {
+    $bg_image_style = ' style="background-image: url(\'' . thumb($page->image($page->cover_image()), ['width' => 800])->url() . '\'); background-position: top right; background-repeat: no-repeat; background-size: contain;"';
+  }
+  ?>
+  <section<?= $bg_image_style ?>>
+
   <div class="row row--internalpadding">
 
     <div class="col-xs-12 col-sm-2">
       <? snippet('logo') ?>
     </div>
 
-    <? if($page->cover_image()->isNotEmpty()): ?>
-      <div class="col-xs-9 col-xs-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-4" style="margin-bottom: -10rem;">
-        <figure class="figure--3by2">
-          <img src="<?= thumb($page->image($page->cover_image()), ['width' => 800])->url() ?>" alt="" />
-        </figure>
-      </div>
-    <? endif ?>
-
-    <div class="col-xs-12 col-sm-10 col-md-9<? if($page->cover_image()->isNotEmpty()) { echo ' col-sm-offset-2'; } ?> u-pt7-sm">
+    <div class="col-xs-12 col-sm-10 col-md-5 u-pt7-sm u-pb2">
       <?= $page->text()->kirbytext() ?>
     </div>
+  </div>
 
-    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+  </section>
 
-      <?
-      $list_options = ($page->list_type()->isNotEmpty() && $page->list_type()->value() == 'large') ? array(
-        'snippet_size' => 150,
-        'item_size_class' => 'col-sm-6',
-        'item_ptop' => ''
-      ) : array(
-        'snippet_size' => 90,
-        'item_size_class' => 'col-sm-6',
-        'item_ptop' => ''
-      );
-      ?>
+  <section id="services">
 
-      <div class="row row--internalpadding list">
-        <? $i = 1; foreach ($page->children()->visible() as $item) : ?>
+    <div class="row row--internalpadding">
 
-          <? snippet('list-item', ['i' => $i, 'item' => $item, 'list_options' => $list_options]) ?>
-          
-        <? $i++; endforeach ?>
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+
+        <?
+        $list_options = ($page->list_type()->isNotEmpty() && $page->list_type()->value() == 'large') ? array(
+          'snippet_size' => 150,
+          'item_size_class' => 'col-sm-6',
+          'item_ptop' => ''
+        ) : array(
+          'snippet_size' => 90,
+          'item_size_class' => 'col-sm-6',
+          'item_ptop' => ''
+        );
+        ?>
+
+        <div class="row row--internalpadding list">
+          <? $i = 1; foreach ($page->children()->visible() as $item) : ?>
+
+            <? snippet('list-item', ['i' => $i, 'item' => $item, 'list_options' => $list_options]) ?>
+            
+          <? $i++; endforeach ?>
+        </div>
+
       </div>
 
     </div>
 
-  </div>
+  </section>
 
 </main>
 
