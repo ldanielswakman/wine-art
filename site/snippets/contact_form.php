6c4c76e12821f089795ae5ca81904ef4cb59b549
L Daniel Swakman <d.swakman@gmail.com>
2017-02-16 18:13:20 -0300

    footer: always show contact form fields

--- site/snippets/contact_form.php
+++ site/snippets/contact_form.php
@@ -8,7 +8,7 @@
     <textarea name="message" id="message" required class="field field--big u-widthfull u-mb1" placeholder="<?= l::get('form_message_placeholder') ?>"></textarea>
   </div>
 
-  <div class="row row--internalpadding<?= (isset($type) && $type === 'item') ? '' : ' i-onevent-appearFromTop'; ?>">
+  <div class="row row--internalpadding">
     <div class="col-xs-12 col-sm-5">
       <div class="field-set">
         <input name="email" type="email" required class="field u-mb1 u-widthfull" placeholder="<?= l::get('form_email_placeholder') ?>">
@@ -28,7 +28,7 @@
     <?= honeypot_field() ?>
 
     <div class="col-xs-12 col-sm-3">
-      <button type="submit" class="button u-mb1 u-widthfull"><?= l::get('form_submit') ?></button>
+      <button type="submit" class="button u-mb1 u-widthfull" disabled><?= l::get('form_submit') ?></button>
     </div>
   </div>
 
