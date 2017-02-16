08bf5b2c1f961429ad7e52b8debac035e79ebbe9
L Daniel Swakman <d.swakman@gmail.com>
2017-02-16 17:32:29 -0300

    added blog link to menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,9 +1,14 @@
 <? // Menu items ?>
 <ul>
   <? foreach ($site->pages()->visible() as $p) : ?>
-    <li>
-      <a href="<?=$p->url() ?>" class="<? e($p->isOpen(), 'isActive') ?>"><?= strtolower($p->title()->html()) ?></a>
-    </li>
+    <? if ($p->template() === 'external'): ?>
+      <li>
+        <a href="<?= $p->forward_url() ?>" target="_blank" class="external"><?= strtolower($p->title()->html()) ?></a></li>
+    <? else : ?>
+      <li>
+        <a href="<?= $p->url() ?>" class="<? e($p->isOpen(), 'isActive') ?>"><?= strtolower($p->title()->html()) ?></a>
+      </li>
+    <? endif ?>
   <? endforeach ?>
 </ul>
 
---
+++ site/templates/external.php
@@ -0,0 +1,1 @@
+<? go($page->external_url());
