82a8946bad9d0ede0ffc49e8659e9a0e24b53fc6
L Daniel Swakman <d.swakman@gmail.com>
2017-04-05 16:16:16 +0200

    updated sitemap

--- site/templates/xmlsitemap.php
+++ site/templates/xmlsitemap.php
@@ -11,12 +11,32 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <?php foreach($pages->index() as $p): ?>
-  <?php if(in_array($p->uri(), $ignore) || in_array($p->parent(), $ignoreChildren)) continue ?>
-  <url>
-    <loc><?= html($p->url()) ?></loc>
-    <lastmod><?= $p->modified('Y-m-d') ?></lastmod>
-    <priority><?= ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
-  </url>
-  <?php endforeach ?>
+  <? foreach($pages->index() as $p): ?>
+    <? if(in_array($p->uri(), $ignore) || in_array($p->parent(), $ignoreChildren)) continue ?>
+    <url>
+      <loc><?= html($p->url()) ?></loc>
+      <lastmod><?= $p->modified('Y-m-d') ?></lastmod>
+      <priority><?= ($p->isHomePage()) ? 1 : number_format(0.75/$p->depth(), 1) ?></priority>
+    </url>
+    <?
+    // retrieve blog posts JSON to list blog posts
+    if($p->template() == 'blog') :
+      $curl = curl_init();
+      curl_setopt_array($curl, array(
+          CURLOPT_RETURNTRANSFER => 1,
+          CURLOPT_URL => $p->url() . '.json',
+      ));
+      $json = curl_exec($curl);
+      $r = json_decode($json, true);
+      curl_close($curl);
+      foreach ($r['data'] as $post) :
+      ?>
+        <url>
+          <loc><?= $p->url() . '/' . $post['slug'] ?></loc>
+          <lastmod><?= date("Y-m-d", strtotime($post['date'])) ?></lastmod>
+          <priority>0.5</priority>
+        </url>
+      <? endforeach ?>
+    <? endif ?>
+  <? endforeach ?>
 </urlset>
