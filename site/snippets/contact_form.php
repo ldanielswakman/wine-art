298e7dc1338d555e850c55230c6d09e9fcffb430
L Daniel Swakman <d.swakman@gmail.com>
2017-04-12 18:49:39 +0200

    bugfix

--- site/snippets/contact_form.php
+++ site/snippets/contact_form.php
@@ -43,7 +43,7 @@
   </div>
 
   <!-- State: success -->
-  <div id="cover_success" class="u-pin-cover u-z2 u-flex-center u-aligncenter <?= ($type === 'item') ? 'bg-greylighter c-dullblue' : 'bg-darkblue'; ?> u-hide">
+  <div id="cover_success" class="u-pin-cover u-z2 u-flex-center u-aligncenter <?= ($type === 'item') ? 'bg-greylighter c-dullblue' : 'bg-darkblue c-white'; ?> u-hide">
     <div>
       <img src="<?= url('assets/images/check.svg') ?>" alt="" />
       <div class="u-w20 u-mt1"><?= (isset($success_msg) && strlen($success_msg) > 1) ? $success_msg : l::get('form_success_msg') ?></div>
