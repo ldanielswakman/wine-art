6c92238e35fa8cca9c2853322d2fcd6a5bfac54e
L Daniel Swakman <d.swakman@gmail.com>
2017-04-13 13:37:34 +0200

    test

--- site/snippets/email-partial-footer.php
+++ site/snippets/email-partial-footer.php
@@ -1,6 +1,6 @@
 <div style="padding: 15px; text-align: center; color: #cad1d9;">
   <p>
     <em>This message was automatically sent from wine-art.co</em><br>
-    <small style="color: #ffffff"><?= date('d M Y H:i:s') ?></small>
+    <span style="display: block; margin-top: 0.25em; color: #ffffff;"><?= date('d M Y H:i:s') ?></span>
   </p>
 </div>
