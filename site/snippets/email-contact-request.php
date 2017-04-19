96db15b55753493f0c8e7b3833af1ddbc0d9a7c0
L Daniel Swakman <d.swakman@gmail.com>
2017-04-19 17:29:26 +0200

    fix

--- site/snippets/email-contact-request.php
+++ site/snippets/email-contact-request.php
@@ -26,7 +26,7 @@
           <td style="padding: 5px 10px; color: #909599;"><? echo ucfirst($field) ?></td>
           <td ><? echo $value ?></td>
         </tr>
-        <? if ($value == 'name') : ?>
+        <? if ($field == 'name') : ?>
           <tr>
             <td style="padding: 5px 10px; color: #909599;">Email</td>
             <td ><? echo $email ?></td>
