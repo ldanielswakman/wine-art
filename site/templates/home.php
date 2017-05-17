24a1fc2fcc3d68a83b9b9528f747aac3a512c3d3
L Daniel Swakman <d.swakman@gmail.com>
2017-05-17 11:33:38 +0200

    fixed logo bug

--- site/templates/home.php
+++ site/templates/home.php
@@ -10,7 +10,7 @@
     <div class="col-xs-12 col-sm-6 col-sm-offset-6">
 
       <? if($logo = $site->logo()): ?>
-        <a href="<?= $site->url() ?>">
+        <a href="<?= $site->url() ?>" class="u-block">
           <img src="<?= $site->image($logo)->url() ?>" class="header__logo" />
         </a>
       <? endif ?>
